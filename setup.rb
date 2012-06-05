6e41e7674b7e751d72ea15d77a5867c607dd16cd
Jeffrey Stokes <jeff@alumni.ecu.edu>
2012-06-05 17:19:17 -0400

    overwrite if symlink exists, create bak file

--- setup.rb
+++ setup.rb
@@ -2,7 +2,7 @@
 # setup.rb
 
 #list of dirs which you don't want to symlink
-ignored = %w(.gitignore .gitmodules setup.rb readme init installer)
+ignored = %w(.gitignore .gitmodules setup.rb README init installer)
 
 current_dir = File.expand_path(Dir.pwd)
 home_dir = File.expand_path("~")
@@ -12,6 +12,11 @@ links = `git ls-tree --name-only HEAD`.lines.map(&:strip).select {|x| !ignored.i
 links.each do |link|
   link = File.join(current_dir, link)
   symlink = File.join(home_dir, File.basename(link))
+
+  if File.exists?(symlink) 
+    File.rename(symlink, "#{symlink}.bak")
+  end
+
   `ln -ns #{link} #{symlink}`
 end
 
