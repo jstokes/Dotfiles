d42c76ae328a4da40e2f9a580045d180ad40884e
Jeff Stokes <jstokes@appia.com>
2014-10-13 12:38:46 -0400

    Update ruby setup script, add new emacs files

--- setup.rb
+++ setup.rb
@@ -5,19 +5,30 @@
 ignored = %w(.gitignore .gitmodules setup.rb README init installer)
 
 current_dir = File.expand_path(Dir.pwd)
-home_dir = File.expand_path("~")
-
 links = `git ls-tree --name-only HEAD`.lines.map(&:strip).select {|x| !ignored.include?(x)  }
 
-links.each do |link|
-  link = File.join(current_dir, link)
-  symlink = File.join(home_dir, File.basename(link))
+def different_locations
+  {'personal' => '/Users/jstokes/.emacs.d/personal',
+   'prelude-modules.el' => '/Users/jstokes/.emacs.d/prelude-modules.el'}
+end
 
-  if File.exists?(symlink) 
+def get_symlink(file_name)
+  if different_locations.include? file_name
+    different_locations[file_name]
+  else
+    home_dir = File.expand_path("~")
+    File.join(home_dir, file_name)
+  end
+end
+
+links.each do |link|
+  symlink = get_symlink(link)
+  link = File.join(current_dir, link)
+
+  if File.exists?(symlink)
     File.rename(symlink, "#{symlink}.bak")
   end
 
   puts "ln -ns #{link} #{symlink}"
   `ln -ns #{link} #{symlink}`
 end
-
