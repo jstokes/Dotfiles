c390317fcca61b00de5fea8c61a5aa170937e265
Jeff Stokes <jeffecu88@gmail.com>
2014-10-14 19:58:48 -0400

    Add pack for emacs live configuration

---
+++ jstokes-pack/config/bindings.el
@@ -0,0 +1,35 @@
+;; Place your bindings here.
+
+;; For example:
+;;(define-key global-map (kbd "C-+") 'text-scale-increase)
+;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+(setq evil-want-C-u-scroll t)
+
+; https://gist.github.com/txus/5420665
+; map jk to ESC
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+(evil-define-command cofi/maybe-exit ()
+  :repeat change
+  (interactive)
+  (let ((modified (buffer-modified-p)))
+    (insert "j")
+    (let ((evt (read-event (format "Insert %c to exit insert state" ?k)
+                           nil 0.5)))
+      (cond
+       ((null evt) (message ""))
+       ((and (integerp evt) (char-equal evt ?k))
+        (delete-char -1)
+        (set-buffer-modified-p modified)
+        (push 'escape unread-command-events))
+       (t (setq unread-command-events (append unread-command-events
+                                              (list evt))))))))
+
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
+(define-key evil-normal-state-map (kbd "+") 'er/expand-region)
+(define-key evil-visual-state-map (kbd "x") 'er/expand-region)
+(define-key evil-visual-state-map (kbd "X") 'er/contract-region)
