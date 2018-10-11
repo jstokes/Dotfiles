5677f46d7ca0b833450207d2731e5a39d5d07ab9
Jeff Stokes <jeff@amperity.com>
2018-10-11 14:42:09 -0400

    setup updates

--- setup.rb
+++ setup.rb
@@ -2,28 +2,19 @@
 # setup.rb
 
 #list of dirs which you don't want to symlink
-ignored = %w(.gitignore .gitmodules setup.rb README init installer .live-packs)
-
+ignored = %w(.gitignore .gitmodules setup.rb README init installer)
 current_dir = File.expand_path(Dir.pwd)
 links = `git ls-tree --name-only HEAD`.lines.map(&:strip).select {|x| !ignored.include?(x)  }
 
-def different_locations
-  me = `whoami`.chomp
-  {'jstokes-pack' => "/Users/#{me}/.live-packs/jstokes-pack"}
-end
-
 def get_symlink(file_name)
-  if different_locations.include? file_name
-    different_locations[file_name]
-  else
-    home_dir = File.expand_path("~")
-    File.join(home_dir, file_name)
-  end
+  home_dir = File.expand_path("~")
+  File.join(home_dir, file_name)
 end
 
 links.each do |link|
   symlink = get_symlink(link)
   link = File.join(current_dir, link)
 
+  puts "linking #{link} -> #{symlink}"
   `ln -nsFv #{link} #{symlink}`
 end
