e7bc7ba51c84aeab66e685f8b4b4472703672e64
Jeff Stokes <jeff@amperity.com>
2024-05-31 13:55:57 -0700

    Much needed cleanup of doom configs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,68 +1,70 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Jeff Stokes"
       user-mail-address "jeffecu88@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-;;
-(setq doom-font (font-spec :family "Iosevka" :size 15))
+;; ## Top Level Keybindings
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-ir-black)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type nil)
+(map!
+ :map doom-leader-map
+ "SPC" 'counsel-M-x
+ "sc" 'evil-ex-nohighlight
+ "yr" 'counsel-yank-pop
+ "ai" 'gptel)
 
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; ## UI Settings
 
-;; they are implemented.
+(setq
+ auto-save-default t
+ display-line-numbers-type nil
+ doom-font (font-spec :family "Iosevka" :size 20)
+ doom-modeline-height 1
+ doom-theme 'doom-ir-black
+ evil-escape-key-sequence "fd"
+ evil-split-window-below t
+ evil-vsplit-window-right t
+ evil-want-fine-undo t
+ +ivy-buffer-preview t
+ ivy-extra-directories nil
+ ivy-use-virtual-buffers t
+ org-directory "~/org/"
+ truncate-string-ellipsis "…"
+ undo-limit 80000000
+ vimish-fold-marks '(";; {" . ";; }")
+ which-key-idle-delay 0.3
+ git-link-default-branch "main")
+
+(after! doom-modeline
+  (setq doom-modeline-persp-name t))
+
+(set-face-attribute 'mode-line nil :family "Iosevka" :height 140)
+
+(when (not (equal "Battery status not available" (battery)))
+  (display-battery-mode 1))
+
+(map!
+ :map ivy-minibuffer-map
+ ;; TAB and RET go into the selected directory or opens the selected file
+ "RET" 'ivy-alt-done
+ "TAB" 'ivy-alt-done
+ ;; C-h goes up a dir (like spacemacs helm)
+ "C-h" 'ivy-backward-delete-char)
+
+
+;; ##Pinentry
+
+(use-package! pinentry
+  :init (setq epa-pinentry-mode `loopback)
+  (pinentry-start))
+
+;; ## Smartparens
 
 (setq smartparens-strict-mode t)
-
 (add-hook 'emacs-lisp-mode-hook #'smartparens-strict-mode)
 (add-hook 'emacs-lisp-mode-hook #'evil-smartparens-mode)
 (add-hook 'clojure-mode-hook #'smartparens-strict-mode)
 (add-hook 'clojure-mode-hook #'evil-smartparens-mode)
-
 
 ;; Sets SPC k <key> to do a smartparens command and enter lisp state
 ;; (Borrowed from Spacemacs)
@@ -86,109 +88,34 @@
   (sp-pair "(" nil :unless '(:rem sp-point-before-word-p)))
 
 
-(map!
- :map doom-leader-map
- ;; SPC SPC = M-x, like in spacemacs
- "SPC" 'counsel-M-x
- ;; SPC sc remove highlight
- "sc" 'evil-ex-nohighlight
- ;; Yank ring
- "yr" 'counsel-yank-pop)
+;; ## Cider/Clojure
 
-
-;; Make recent files (that aren't buffers yet) appear in SPC b b
-(setq ivy-use-virtual-buffers t)
-
-;; Hide . and ..
-(setq ivy-extra-directories nil)
-
-(map!
- :map ivy-minibuffer-map
- ;; TAB and RET go into the selected directory or opens the selected file
- "RET" 'ivy-alt-done
- "TAB" 'ivy-alt-done
- ;; C-h goes up a dir (like spacemacs helm)
- "C-h" 'ivy-backward-delete-char)
-
-;; Make leader key help menu show up quicker
-(setq which-key-idle-delay 0.3)
-
-;; Set cljstyle formatter
-(set-formatter!
-  'cljstyle "/usr/local/bin/cljstyle pipe"
-  :modes '(clojure-mode clojurescript-mode clojurec-mode))
-
-
-(add-hook! 'before-save-hook #'+format/buffer)
-
-;; (remove-hook! 'before-save-hook #'+format/buffer)
-
-;; (setq-hook! 'clojure-mode-hook +format-with-lsp nil)
-;; (setq-hook! 'clojurec-mode-hook +format-with-lsp nil)
-;; (setq-hook! 'clojurescript-mode-hook +format-with-lsp nil)
-
-;; fd to escape
-(setq evil-escape-key-sequence "fd")
+(setq-default cider-use-overlays t)
 
 (after! (:or clojure-mode clojurescript-mode)
   (setq clojure-indent-style 'align-arguments)
   (setq cider-default-cljs-repl 'shadow)
-  (setq cider-print-fn 'puget)
+  (setq cider-print-fn 'fipp)
+  (setq cider-print-options '(("print-length" 50)))
+  (setq cider-print-quota 524288)
   (setq cider-shadow-cljs-parameters "server -d djblue/portal:0.9.0")
-  (setq cider-enrich-classpath t))
+  (setq cider-enrich-classpath t)
+  (setq cider-use-overlays t)
+  (setq cider-save-file-on-load nil)
+  (setq cider-overlays-use-font-lock t))
 
-;; don't close my repl window plz
 (after! cider
   (set-popup-rule! "^*cider-repl" :quit nil :ttl nil))
 
+(set-formatter!
+  'cljstyle '("cljstyle" "pipe")
+  :modes '(clojure-mode clojurescript-mode clojurec-mode))
 
-;; magit cfg
-;; (add-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-pushremote)
-;; (add-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent)
-;; (add-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-pushremote)
-;; (add-hook 'magit-status-sections-hook 'magit-insert-unpulled-from-upstream)
-;; (add-hook 'magit-status-sections-hook 'magit-insert-stashes)
-
-
-;; Doom modeline
-(setq doom-modeline-height 1)
-(set-face-attribute 'mode-line nil :family "Iosevka" :height 140)
-
-(after! doom-modeline
-  (setq doom-modeline-persp-name t))
-
-;; (after! lsp-mode
-;;   (setq lsp-enable-file-watchers nil
-;;         lsp-enable-indentation nil
-;;         lsp-ui-doc-show-with-cursor t
-;;         lsp-ui-sideline-show-code-actions nil
-;;         lsp-modeline-code-actions-enable nil
-;;         lsp-completion-enable-additional-text-edit nil
-;;         lsp-ui-doc-enable t
-;;         lsp-lens-enable t
-;;         lsp-enable-symbol-highlighting nil
-;;         lsp-signature-render-documentation t))
-
-
-(setq undo-limit 80000000           ;; Undo limit 80mb
-      evil-want-fine-undo t         ;; More graunular undo for evil
-      auto-save-default t           ;; Nobody likes to loose work, I certainly don't
-      truncate-string-ellipsis "…") ;; Unicode ellispis are nicer than "...", and also save /precious/ space
-
-;; Show battery when battery powered
-(when (not (equal "Battery status not available" (battery)))
-  (display-battery-mode 1))
-
-
-;; Split to the right and below
-(setq evil-vsplit-window-right t
-      evil-split-window-below t)
-
-;; Preview buffers when opening splits
-(setq +ivy-buffer-preview t)
-
-;; git-link should default to `main' branch
-(setq git-link-default-branch "main")
+(setq +format-on-save-enabled-modes
+      '(clojure-mode
+        clojurescript-mode
+        clojurec-mode
+        (not markdown-mode)))
 
 ;; clj-refactor keybindings
 (map!
@@ -238,40 +165,36 @@
   :n  "r u w"  #'cljr-unwind))
 
 
-;; (map!
-;;  :after lsp-mode
-;;  (:leader
-;;   :n  "l"      nil
-;;   :n  "l l"    #'lsp-ui-imenu
-;;   :n  "l a i"  #'lsp-clojure-add-import-to-namespace
-;;   :n  "l a m"  #'lsp-clojure-add-missing-libspec
-;;   :n  "l c c"  #'lsp-clojure-cycle-coll
-;;   :n  "l c n"  #'lsp-clojure-clean-ns
-;;   :n  "l c p"  #'lsp-clojure-cycle-privacy
-;;   :n  "l e f"  #'lsp-clojure-extract-function
-;;   :n  "l e l"  #'lsp-clojure-expand-let
-;;   :n  "l f u"  #'lsp-find-references
-;;   :n  "l i l"  #'lsp-clojure-introduce-let
-;;   :n  "l i s"  #'lsp-clojure-inline-symbol
-;;   :n  "l m l"  #'lsp-clojure-move-to-let
-;;   :n  "l r l"  #'lsp-clojure-expand-let
-;;   :n  "l r s"  #'lsp-rename
-;;   :n  "l s n"  #'lsp-clojure-clean-ns
-;;   :n  "l t f"  #'lsp-clojure-thread-first-all
-;;   :n  "l t h"  #'lsp-clojure-thread-first
-;;   :n  "l t l"  #'lsp-clojure-thread-last-all
-;;   :n  "l u a"  #'lsp-clojure-unwind-all
-;;   :n  "l u w"  #'lsp-clojure-unwind-thread))
+;; ## Magic/AI
 
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
-;; Admin repl!
-(defun admin-repl (cluster)
-  (interactive "sCluster: ")
-  (message (format "Connecting to %s." cluster))
-  (shell-command (format "~/work/app/bin/admin-repl %s start" cluster))
-  (cl-destructuring-bind (host port) (split-string
-                                      (shell-command-to-string
-                                       (format "~/work/app/bin/admin-repl %s addr" cluster)))
-    (cider-connect-clj (list :host host
-                             :port port
-                             :project-dir "~/work/app/service/admin/admin-repl"))))
+(setq copilot-indent-offset-warning-disable t)
+
+(use-package! gptel)
+
+(defun my/company-complete-or-next ()
+  "Complete the current selection or select the next item."
+  (interactive)
+  (cond ((not company-candidates) ;; no popup is active
+         (company-manual-begin))
+        ((eq company-selection nil) ;; no completion is selected
+         (company-select-first)
+         (company-complete-selection))
+        (t ;; a completion is selected
+         (company-complete-selection))))
+
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "C-SPC") 'my/company-complete-or-next))
+
+(with-eval-after-load 'evil
+  (define-key evil-insert-state-map (kbd "C-SPC") 'my/company-complete-or-next))
+
+;; Load any host specific settings if they exist
+(load! "host.el" nil t)
